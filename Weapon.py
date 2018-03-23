588bdd52a61fe1c8201da7437c7a0a51bcd331a1
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-22 21:21:13 -0400

    Update Weapon.py

--- Weapon.py
+++ Weapon.py
@@ -6,18 +6,22 @@ class Weapon:
     upperMod = 1
     name = "Weapon"
 
+    #Creates Constructor
     def __init__(self, uses, lowerMod, upperMod, name):
         self.usesLeft = uses
         self.lowerMod = lowerMod
         self.upperMod = upperMod
         self.name = name
-
+    
+    #minuses a use from weapons when they are used
     def use(self):
         self.usesLeft -= 1
         return random.uniform(self.lowerMod, self.upperMod)
-
+    
+    #returns the amount of uses left on a weapon
     def getUsesLeft(self):
         return self.usesLeft
 
+    #returns name of a weapon
     def getName(self):
         return self.name
