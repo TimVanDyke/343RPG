0c1a8d5ee8424357c25d8b05106e8fd4e48868fd
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-22 21:44:31 -0400

    Update NPC.py

--- NPC.py
+++ NPC.py
@@ -4,12 +4,18 @@ class NPC(Observable):
     maxAtt = 1
     minAtt = 1
     health = 1
-
+    
+    #Creates Constructor for NPCs
+    #@Param self
+    #   Current Instance
     def __init__(self, minAtt, maxAtt, minHealth, maxHealth):
         self.maxAtt = maxAtt
         self.minAtt = minAtt
         self.health = random.uniform(minHealth, maxHealth)
 
+    #Allows for monster to attack with a chance for critical hit
+    #@Param self
+    #   Current Instance
     def attack(self):
         att = random.uniform(self.minAtt, self.maxAtt)
 
@@ -21,9 +27,19 @@ class NPC(Observable):
         else:
             return att * 3
 
+    #Monster takes damage depending on damage and weapon passed
+    #@Param self
+    #   Current Instance
+    #@Param damage
+    #   Damage that NPC will be taking
+    #@Param Weapon
+    #   Weapon that the player currently holds
     def takeDamage(self, damage, Weapon):
         pass
-
+    
+    #Return true if player is alive and false if player is dead
+    #@Param self
+    #   Current Instance
     def isAlive(self):
         if self.health > 1:
             return True
