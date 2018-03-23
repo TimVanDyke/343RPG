9187f9d5a0a2c93b1f1b9861ba3f3d2e7b0d6a3c
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-22 21:22:46 -0400

    Update Kiss.py

--- Kiss.py
+++ Kiss.py
@@ -2,7 +2,11 @@ from Weapon import Weapon
 import random
 
 class Kiss(Weapon):
+    
+    #Creates Constructor for Kisses
     def __init__(self):
         super(Weapon, self).__init__(1, 1, 1, "Kiss")
+        
+    #Uses the weapon and sets a random damage amount
     def use(self):
         return random.uniform(self.lowerMod, self.upperMod)
