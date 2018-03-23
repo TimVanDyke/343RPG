8042fe3153307bcaf0e6419498ea8e1232b31419
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-22 21:27:38 -0400

    Update Vampire.py

--- Vampire.py
+++ Vampire.py
@@ -2,9 +2,11 @@ from Monster import Monster
 
 class Vampire(Monster):
 
+    #Creates Constructor for Vampires
     def __init__(self):
         super(Monster, self).__init__(0, 20, 100, 200)
 
+    #Vampires do not take any damage from Chocolate Bars
     def takeDamage(self, damage, weapon):
         if weapon.getName == "ChocolateBars":
             damage = 0
