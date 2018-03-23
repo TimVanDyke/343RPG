2065b8f58c03368748279b61d7e5557a14a52305
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-22 21:37:58 -0400

    Update Ghoul.py

--- Ghoul.py
+++ Ghoul.py
@@ -3,10 +3,18 @@ from Monster import Monster
 class Ghoul(Monster):
 
     #Creates Constructor for Ghouls
+    #@Param Self 
+    #   Current Instance
     def __init__(self):
         super(Monster, self).__init__(15, 30, 40, 80)
 
     #Ghouls take a different amount of damage from Nerd Bombs than other weapons
+    #@Param Self
+    #   Current Instance
+    #@Param damage
+    #   How much damage the enemy is taking
+    #@Param weapon
+    #   Passes which weapon is hitting them
     def takeDamage(self, damage, weapon):
         if weapon.getName == "NerdBombs":
             damage = damage * 5
