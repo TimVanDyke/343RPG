b1733153b80361e2d8c3c4eac700d1ee28bf96f9
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-22 21:23:11 -0400

    Update NerdBombs.py

--- NerdBombs.py
+++ NerdBombs.py
@@ -1,5 +1,7 @@
 from Weapon import Weapon
 
 class NerdBombs(Weapon):
+    
+    #Creates Constructor for NerdBombs
     def __init__(self):
         super(Weapon, self).__init__(1, 3.5, 5, "NerdBombs")
