2c068ea3dcd3144de63bb40a3fff0231079a6a98
TimVanDyke <vandykti@mail.gvsu.edu>
2018-03-23 16:21:11 -0400

    *Actually fixing Neighborhood*

--- Neighborhood.py
+++ Neighborhood.py
@@ -20,7 +20,7 @@ class Neighborhood():
         while not moved:
             dir = str(input("please give a direction in quotes: (w for up, a for left, s for down, d for right)"))
             if dir == "w":
-                if self.getPos[1] == 0:
+                if self.getPos()[1] == 0:
                     print("you are already at the top of the Neighborhood! We need to finish our own Neighborhood first!")
                 else:
                     self.setPos(0, -1)
@@ -32,7 +32,7 @@ class Neighborhood():
                     moved = True
                     return False
             elif dir == "a":
-                if self.getPos[0] == 0:
+                if self.getPos()[0] == 0:
                     print("you are already at the left side of the Neighborhood! We need to finish our own Neighborhood first!")
                 else:
                     self.setPos(-1, 0)
@@ -44,7 +44,7 @@ class Neighborhood():
                     moved = True
                     return False
             elif dir == "s":
-                if self.getPos[1] == self.h - 1:
+                if self.getPos()[1] == self.h - 1:
                     print("you are already at the bottom of the Neighborhood! We need to finish our own Neighborhood first!")
                 else:
                     self.setPos(0, 1)
@@ -56,7 +56,7 @@ class Neighborhood():
                     moved = True
                     return False
             elif dir == "d":
-                if self.getPos[0] == self.w - 1:
+                if self.getPos()[0] == self.w - 1:
                     print("you are already at the right side of the Neighborhood! We need to finish our own Neighborhood first!")
                 else:
                     self.setPos(1, 0)
