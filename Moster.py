7fe69f6c52aee0d957aa1f3e89ddd2cacee47d0f
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-22 21:40:41 -0400

    Update Moster.py

--- Moster.py
+++ Moster.py
@@ -3,8 +3,12 @@ from NPC import NPC
 class Monster(NPC):
     
     #Creates Constructor for Monsters
+    #@Param self
+    #   Current Instance
     def __init__(self):
         super(NPC, self).__init__(1, 1, 1, 1)
-        
+    
+    #@Param self
+    #   Current Instance
     def die(self):
         return "FIXME"
