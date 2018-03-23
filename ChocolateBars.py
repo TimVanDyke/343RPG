3b83fe734d2c21e6a44d010fddbfd0ba8dcab085
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-22 21:38:18 -0400

    Update ChocolateBars.py

--- ChocolateBars.py
+++ ChocolateBars.py
@@ -4,5 +4,6 @@ class ChocolateBars(Weapon):
     
     #Creates Constructor for ChocolateBars
     #@Param Self
+    #   Current Instance
     def __init__(self):
         super(Weapon, self).__init__(4, 2, 2.4, "ChocolateBars")
