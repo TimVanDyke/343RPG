33cb3a4d79b77d988cc25a1b5b1c877e4b401b69
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-22 21:52:03 -0400

    Update SourStraws.py

--- SourStraws.py
+++ SourStraws.py
@@ -3,5 +3,7 @@ from Weapon import Weapon
 class SourStraws(Weapon):
     
     #Creates Constructor for SourStraws
+    #@Param self
+    #   Current Instance
     def __init__(self):
         super(Weapon, self).__init__(2, 1, 1.75, "sourStraws")
