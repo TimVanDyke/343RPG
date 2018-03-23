4607b2a3aebd98d520de68b7f1e67739fa5ce268
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-22 21:29:58 -0400

    Update Zombie.py

--- Zombie.py
+++ Zombie.py
@@ -2,9 +2,11 @@ from Monster import Monster
 
 class Zombie(Monster):
 
+    #Creates Constructor for SourStraws
     def __init__(self):
         super(Monster, self).__init__(0, 10, 50, 100)
-
+    
+    #Zombies take double the damage when hit with sour straws
     def takeDamage(self, damage, weapon):
         if weapon.getName == "SourStraws":
             damage = damage * 2
