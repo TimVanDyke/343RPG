76a2f5e969409f9946b4e7d00b713705942bdf13
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-22 21:46:12 -0400

    Update Person.py

--- Person.py
+++ Person.py
@@ -5,6 +5,5 @@ class Person(NPC):
     #Creates Constructor for Person
     #@Param self
     #   Current Instance
-    
     def __init__(self):
         super(NPC, self).__init__(-1, -5, 100, 100)
