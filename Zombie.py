1c6cc89b2ed9ec8a6ee8cbbe31c2bd65531ede91
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-24 16:30:57 -0400

    Update Zombie.py

--- Zombie.py
+++ Zombie.py
@@ -4,6 +4,8 @@ import random
 class Zombie(Monster):
 
     #Creates Constructor for SourStraws
+    #@Param Self
+    #   Current Instance
     def __init__(self):
         super(Monster, self).__init__(0, 10, 50, 100)
         self.maxAtt = 0
@@ -12,6 +14,10 @@ class Zombie(Monster):
         self.health = random.randint(50, 100)
 
     #Zombies take double the damage when hit with sour straws
+    #@Param damage
+    #    Damage passed from NPC  
+    #@Param weapon
+    #    Weapon passed from Player
     def takeDamage(self, damage, weapon):
         if weapon == "SourStraws":
             damage = damage * 2
