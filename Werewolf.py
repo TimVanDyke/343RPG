259a9d7545fedf00d50b11e1f7bb66fd5f478b0c
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-24 16:29:41 -0400

    Update Werewolf.py

--- Werewolf.py
+++ Werewolf.py
@@ -3,13 +3,22 @@ from Monster import Monster
 class Werewolf(Monster):
 
     #Creates Constructor for Werewolves
+    #@Param self
+    #   Current Instance
     def __init__(self):
         super(Monster, self).__init__(0, 40, 200, 200)
         self.maxAtt = 0
         self.minAtt = 40
         self.health = 200
         self.name = "Werewolf"
+        
     #Werewolves don't take damage from Chocolate Bars or Sour Straws
+    #@Param self
+    #   Current Instance
+    #@Param Damage
+    #   passed damage from NPC
+    #@Param Weapon
+    #   passed weapon from player
     def takeDamage(self, damage, weapon):
         if weapon == "ChocolateBars":
             damage = 0
