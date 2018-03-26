82d5b4fe83432791f1d0d4d44c291b5381c153b4
AndrewOverman <AndrewOverman0517@gmail.com>
2018-03-25 22:33:22 -0400

    Update Neighborhood.py

--- Neighborhood.py
+++ Neighborhood.py
@@ -33,7 +33,7 @@ class Neighborhood():
                 else:
                     self.setPos(0, 1)
                     self.Matrix[i][j + 1]
-										alive = self.getHomeAttack(player)
+		    alive = self.getHomeAttack(player)
                     if alive is False:
                         return True
                     else:
@@ -45,7 +45,7 @@ class Neighborhood():
                     print("you are already at the left side of the Neighborhood! We need to finish our own Neighborhood first!")
                 else:
                     self.setPos(-1, 0)
-										self.Matrix[i - 1][j]
+		    self.Matrix[i - 1][j]
                     alive = self.getHomeAttack(player)
                     if alive is False:
                         return True
@@ -58,7 +58,7 @@ class Neighborhood():
                     print("you are already at the bottom of the Neighborhood! We need to finish our own Neighborhood first!")
                 else:
                     self.setPos(0, 1)
-										self.Matrix[i][j - 1]
+		    self.Matrix[i][j - 1]
                     alive = self.getHomeAttack(player)
                     if alive is False:
                         return True
@@ -71,7 +71,7 @@ class Neighborhood():
                     print("you are already at the right side of the Neighborhood! We need to finish our own Neighborhood first!")
                 else:
                     self.setPos(1, 0)
-										self.Matrix[i + 1][j]
+		    self.Matrix[i + 1][j]
                     alive = self.getHomeAttack(player)
                     if alive is False:
                         return True
